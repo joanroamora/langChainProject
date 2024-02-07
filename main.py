6ddcbe62bfae591426447e45c29b30981e969916
nombre <xx@yy.com>
2024-02-06 21:37:30 -0500

    document loader working

--- main.py
+++ main.py
@@ -1,6 +1,21 @@
+                    
 from langchain_community.document_loaders import PyPDFLoader
+from langchain.text_splitter import CharacterTextSplitter
+import tiktoken
 
 loader = PyPDFLoader("pdfs/prueba.pdf")
 pages = loader.load_and_split()
 
 print(pages[0])
+
+print(pages[1])
+
+print(len(pages))
+
+
+#text_splitter = CharacterTextSplitter.from_tiktoken_encoder(
+#    chunk_size=500, chunk_overlap=50
+#)
+#texts = text_splitter.split_text(pages)
+
+#print(texts[0])
