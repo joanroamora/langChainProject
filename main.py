619fdb8d4e6deb2f89303dd0e58f00cd7261f04a
nombre <xx@yy.com>
2024-02-06 20:54:28 -0500

    reestructuring the project

--- main.py
+++ main.py
@@ -1,10 +1,6 @@
-import os
-import sys
-from langchain.schema import Document
-
 from langchain_community.document_loaders import PyPDFLoader
 
-loader = PyPDFLoader("pdfs/Ley_1437_de_2011.pdf")
+loader = PyPDFLoader("pdfs/prueba.pdf")
 pages = loader.load_and_split()
 
 print(pages[0])
