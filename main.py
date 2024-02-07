0c8192f9ffc0e5094d04b1b415285341878cf370
nombre <xx@yy.com>
2024-02-07 14:58:41 -0500

    changing the chunk configuratiokn

--- main.py
+++ main.py
@@ -13,8 +13,8 @@ print(len(pages))
 
 text_splitter = RecursiveCharacterTextSplitter(
     # Set a really small chunk size, just to show.
-    chunk_size=500,
-    chunk_overlap=75,
+    chunk_size=200,
+    chunk_overlap=20,
     length_function=len,
     is_separator_regex=False,
 )
